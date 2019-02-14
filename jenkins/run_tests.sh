02ee1ffe294f5d875fed9b99af104830309400bc
SilentFrogNet <info@silentfrog.net>
2019-02-14 14:17:36 +0100

    updated setup

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -8,9 +8,9 @@ fi
 # Create virtualenv and install necessary packages
 python3 -m venv $PYENV_HOME	# try to use python's builtin venv
 source $PYENV_HOME/bin/activate
-pip3 install --upgrade pip
-pip3 install pylint
-pip3 install pytest
-pip3 install $WORKSPACE/  # where your setup.py lives
+pip3 install --quiet --upgrade pip
+pip3 install --quiet pylint
+pip3 install --quiet pylint
+pip3 install --quiet $WORKSPACE/  # where your setup.py lives
 py.test --verbose --junit-xml test-reports/results.xml mercurius/tests
 # pylint -f parseable mercurius/ | tee pylint.out
