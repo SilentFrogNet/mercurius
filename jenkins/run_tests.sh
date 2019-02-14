ead7dfec023c6047198cdabbd2d3152bb61c7271
SilentFrogNet <info@silentfrog.net>
2019-02-14 15:07:46 +0100

    updated jenkins with pylint

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -7,11 +7,11 @@ fi
 
 # Create virtualenv and install necessary packages
 python -m venv $PYENV_HOME	# try to use python's builtin venv
-source $PYENV_HOME/bin/activate
+. $PYENV_HOME/bin/activate
 pip install --quiet --upgrade pip
 pip install --quiet pycryptodome
 pip install --quiet pylint
 pip install --quiet pytest
 pip install --quiet -e $WORKSPACE/  # where your setup.py lives
 py.test --verbose --junit-xml test-reports/results.xml mercurius/tests
-# pylint -f parseable mercurius/ | tee pylint.out
+pylint -f parseable mercurius/ | tee pylint.out
