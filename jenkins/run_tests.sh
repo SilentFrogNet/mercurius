35214bd7437e37776b1f14fa3460ef3db10a76fd
SilentFrogNet <info@silentfrog.net>
2019-02-14 12:31:14 +0100

    edited run test

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -8,7 +8,7 @@ fi
 # Create virtualenv and install necessary packages
 python3 -m venv $PYENV_HOME	# try to use python's builtin venv
 source $PYENV_HOME/bin/activate
-pip install --upgrade pip
+pip install --quiet --upgrade pip
 pip install --quiet pylint
 pip install --quiet $WORKSPACE/  # where your setup.py lives
 py.test --verbose --junit-xml test-reports/results.xml mercurius/tests
